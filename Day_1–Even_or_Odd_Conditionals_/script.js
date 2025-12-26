4a3139be672aeeb519071f18ec8008b4dd7806dd
amin10003 <maxamedxasancali10003@gmail.com>
2025-12-26 07:28:39 +0300

    Day 1: Even or Odd

---
+++ Day_1–Even_or_Odd_Conditionals_/script.js
@@ -0,0 +1,11 @@
+console.log('conditional and modulo')
+
+function checkIfEven(num){
+if(num % 2 === 0){
+    console.log(`${num} is even number`)
+}else{
+    console.log(`${num} is odd number`)
+}
+}
+
+checkIfEven(74)
